c52c24ef576befd2fd26fb961639e5a17f1439cd
apichonsiit <apichon@siit.tu.ac.th>
2022-11-09 08:32:07 +0700

    fix

--- TasksTrackerBasic/TasksTracker.WebPortal.Frontend.Ui/Program.cs
+++ TasksTrackerBasic/TasksTracker.WebPortal.Frontend.Ui/Program.cs
@@ -31,9 +31,9 @@ namespace TasksTracker.WebPortal.Frontend.Ui
             //Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                // app.UseExceptionHandler("/Error");
+                // // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                // app.UseHsts();
             }
 
             app.UseHttpsRedirection();
